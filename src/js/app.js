0daaa8982a8a2191e7a3b45efe456378d2baa7ed
Junmark Chi <chijunmark97@gmail.com>
2022-06-10 18:47:51 +0800

    Update js file and animations

--- src/js/app.js
+++ src/js/app.js
@@ -88,13 +88,13 @@ class Chess {
             INTRO_BUTTON.classList.add('expansion-1')
             INTRO_BUTTON.innerHTML = ''
             INTRO_BUTTON.style.cursor = 'default'
-            const cellSound = new Audio('../.././public/assets/cell.wav')
+            const cellSound = new Audio('../.././public/media/assets/audio/cell.wav')
             
             setTimeout(() => {  
                 cellSound.load()
                 cellSound.loop = true
                 cellSound.playbackRate = 8.4
-                // cellSound.volume = 0
+                cellSound.volume = 1
                 cellSound.play()
 
                 setTimeout(() => {
@@ -120,6 +120,12 @@ class Board {
             boardCells: 64,
             // boardData: []
         }
+
+        this.currentPlayer = ''
+        this.player1Name = ''
+        this.player2Name = ''
+        this.player1Color = ''
+        this.player2Color = ''
     }
 
     createChessBoard(INTRO_BUTTON){
@@ -166,50 +172,92 @@ class Board {
         const shoulder1 = create('div')
         const shoulder2 = create('div')
         const shoulder3 = create('div')
-        const screen = create('div')
+        const screen1 = create('div')
+        const screen2 = create('div')
         const h1P1Name = create('h1')
+        const h1P2Name = create('h1')
         const inputP1Name = create('input')
+        const inputP2Name = create('input')
         const p1Select = create('h1')
+        const p2Select = create('h1')
         const p1WB = create('div')
+        const p2WB = create('div')
         const p1WhiteButton = create('button')
         const p1BlackButton = create('button')
+        const p2WhiteButton = create('div')
+        const p2BlackButton = create('div')
+        const startGame = create('button')
 
         h1P1Name.innerHTML = `Player 1 Name:`
+        h1P2Name.innerHTML = `Player 2 Name:`
         p1Select.innerHTML = `Select Color`
+        p2Select.innerHTML = `Auto-Selected Color`
         p1WhiteButton.innerHTML = `<i class="fa-regular fa-chess-queen"></i>`
         p1BlackButton.innerHTML = `<i class="fa-solid fa-chess-queen"></i>`
-
+        p2WhiteButton.innerHTML = `<i class="fa-regular fa-chess-queen"></i>`
+        p2BlackButton.innerHTML = `<i class="fa-solid fa-chess-queen"></i>`
+        startGame.innerHTML = `Start Game`
+        
         inputP1Name.setAttribute('autocomplete', 'off')
+        inputP1Name.setAttribute('type', 'text')
+        inputP1Name.setAttribute('spellcheck', 'false')
+        inputP2Name.setAttribute('autocomplete', 'off')
+        inputP2Name.setAttribute('type', 'text')
+        inputP2Name.setAttribute('spellcheck', 'false')
 
         arm.classList.add('arm')
         armJoint.classList.add('arm-joint')
         shoulder1.classList.add('shoulder1')
         shoulder2.classList.add('shoulder2')
         shoulder3.classList.add('shoulder3')
-        screen.classList.add('screen')
-        h1P1Name.classList.add('h1-name')
-        inputP1Name.classList.add('input-name')
+        screen1.classList.add('screen-1')
+        screen2.classList.add('screen-2')
+        h1P1Name.classList.add('h1-name1')
+        h1P2Name.classList.add('h1-name2')
+        inputP1Name.classList.add('input-name1')
+        inputP2Name.classList.add('input-name2')
         p1Select.classList.add('p1-select')
+        p2Select.classList.add('p2-select')
         p1WB.classList.add('p1-WB')
+        p2WB.classList.add('p2-WB')
         p1WhiteButton.classList.add('p1-Wbutton')
         p1BlackButton.classList.add('p1-Bbutton')
+        p2WhiteButton.classList.add('p2-Wbutton')
+        p2BlackButton.classList.add('p2-Bbutton')
+        startGame.classList.add('start-game')
 
         setTimeout(()=> {
             armJoint.style.display = 'flex'
         },3500)
 
         p1WhiteButton.addEventListener('click', () => {
-            shoulder3.style = `animation-direction: alternate-reverse;`
-            shoulder2.style = `animation-direction: alternate-reverse;`
-            shoulder1.style = `animation-direction: alternate-reverse;`
-            armJoint.style = `animation-direction: alternate-reverse;`
+            if(inputP1Name.value === ''){
+                return inputP1Name.select()
+            } else {
+                screen1.style.display = 'none'
+                screen2.style.display = 'flex'
+                this.player1Name = inputP1Name.value
+                this.currentPlayer = 'white'
+                this.player1Color = 'white'
+                p2WhiteButton.style.display = 'none'
+                console.log(this.currentPlayer)
+                console.log(this.player1Name)
+            }
         })
 
         p1BlackButton.addEventListener('click', () => {
-            shoulder3.style = `animation-direction: reverse;`
-            shoulder2.style = `animation-direction: reverse;`
-            shoulder1.style = `animation-direction: reverse;`
-            armJoint.style = `animation-direction: reverse;`
+            if(inputP1Name.value === ''){
+                return inputP1Name.select()
+            } else {
+                screen1.style.display = 'none'
+                screen2.style.display = 'flex'
+                this.player1Name = inputP1Name.value
+                this.currentPlayer = 'black'
+                this.player1Color = 'black'
+                p2BlackButton.style.display = 'none'
+                console.log(this.currentPlayer)
+                console.log(this.player1Name)
+            }
         })
 
         INTRO_BUTTON.append(arm)
@@ -217,9 +265,26 @@ class Board {
         armJoint.append(shoulder1)
         shoulder1.append(shoulder2)
         shoulder2.append(shoulder3) 
-        shoulder3.append(screen)
-        screen.append(h1P1Name,inputP1Name,p1Select,p1WB)
+        shoulder3.append(screen1,screen2)
+        screen1.append(h1P1Name,inputP1Name,p1Select,p1WB)
+        screen2.append(h1P2Name,inputP2Name,p2Select,p2WB,startGame)
         p1WB.append(p1WhiteButton,p1BlackButton)
+        p2WB.append(p2WhiteButton,p2BlackButton)
+
+    
+        startGame.addEventListener('click', () => {
+            if(inputP2Name.value === ''){
+                return inputP2Name.select()
+            } else {
+                this.player2Name = inputP2Name.value
+                shoulder3.style = `animation: height3reverse 0.5s ease-in-out 0s 1 reverse backwards,idle3 1s ease-in-out infinite alternate;`
+                shoulder2.style = `animation: height2reverse 0.5s ease-in-out 0.5s 1 reverse backwards,idle2 1s ease-in-out infinite alternate`
+                shoulder1.style = `animation: height1reverse 0.5s ease-in-out 1s 1 reverse backwards,idle1 1s ease-in-out infinite alternate`
+                armJoint.style = `animation: growJointreverse 0.5s ease-in-out 1.5s 1 reverse backwards;`
+                arm.style = `animation: growArmreverse 0.5s ease-in-out 2s 1 reverse backwards;`
+                screen2.style = `animation: height3reverse 0.5s ease-in-out 0s 1 reverse backwards,idle3 1s ease-in-out infinite alternate;`
+            }
+        })
     }
 }
 
